8b44256b1d251f8f4a92ca294175a9a533fb2f5a
Ankit-47 <omkathet218@gmail.com>
2024-05-14 19:20:21 +0545

    formand question with css

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './quiz.css'
+import { data } from '../../assets/data';
 
 const Quiz = () => {
+    let [index,setIndex]=useState(0);
+    let[question,setQuestion] = useState(data[index]);
+
+    const checkAns =(e,ans) =>{
+        if(question.ans===ans){
+            e.target.classList.add("correct")
+        }
+        else{
+            e.target.classList.add("wrong");
+        }
+    }
   return (
-    <div>
+    <div className='container'>
       <h1>Quiz App</h1>
       <hr />
-      <h2>Which is required for the Internet connection?</h2>
+      <h2>{index+1}.{question.question}</h2>
       <ul>
-        <li>Modem</li>
-        <li>Router</li>
-        <li>LAN Cable</li>
-        <li>Pen Drive</li>
+        <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+        <li  onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+        <li  onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+        <li  onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+        
       </ul>
       <button>Next</button>
       <div className="index">1 of 5 questions</div>
